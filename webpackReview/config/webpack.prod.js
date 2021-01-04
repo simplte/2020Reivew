79720a16f41fee771154ccfca9f2520f59bde6bd
bqc <6575734@qq.com>
2021-01-04 21:35:32 +0800

    环境区分

--- webpackReview/config/webpack.prod.js
+++ webpackReview/config/webpack.prod.js
@@ -1,11 +1,17 @@
-const {merge}  = require("webpack-merge");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const common  = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common');
+const webpack = require('webpack');
+
 module.exports = merge(common, {
-    mode: 'production',
-    plugins: [
-        new UglifyJsPlugin({
-            sourceMap: true
-        })
-    ]
-})
+	devtool: 'source-map',
+	mode: 'production',
+	plugins: [
+		new UglifyJsPlugin({
+			sourceMap: true
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
+	]
+});
--- webpackReview/src/print.js
+++ webpackReview/src/print.js
@@ -1,4 +1,5 @@
 export default function printMe() {
+    console.log(process.env.NODE_ENV)
     console.log('热更新了，你好我叫bqc11')
     // console.error('出错啦')
 }
